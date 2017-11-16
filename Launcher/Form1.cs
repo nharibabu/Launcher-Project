24b7b13b9a9ea2321f72fa6951763fdf9a8a94df
Daniel, Alex <adaniel20@csc.com>
2017-11-16 19:22:43 +0530

    Added Message Box

--- Launcher/Form1.cs
+++ Launcher/Form1.cs
@@ -24,6 +24,7 @@ namespace Launcher
         {
             Counter = Counter + 1;
             lblCount.Text = Counter.ToString();
+            MessageBox.Show(Counter.ToString());
         }
     }
 }
