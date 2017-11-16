bc05ef16ddcf82b98965e2c9825b2b9f059a993e
Alex Daniel <adaniel20@csc.com>
2017-11-16 18:48:19 +0530

    Add project files.

---
+++ Launcher/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Launcher
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdSubmit = new System.Windows.Forms.Button();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmdSubmit
+            // 
+            this.cmdSubmit.Location = new System.Drawing.Point(106, 203);
+            this.cmdSubmit.Name = "cmdSubmit";
+            this.cmdSubmit.Size = new System.Drawing.Size(75, 23);
+            this.cmdSubmit.TabIndex = 0;
+            this.cmdSubmit.Text = "Increment";
+            this.cmdSubmit.UseVisualStyleBackColor = true;
+            this.cmdSubmit.Click += new System.EventHandler(this.cmdSubmit_Click);
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(129, 68);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(13, 13);
+            this.lblCount.TabIndex = 1;
+            this.lblCount.Text = "0";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 265);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.cmdSubmit);
+            this.Name = "FrmMain";
+            this.Text = "Launcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdSubmit;
+        private System.Windows.Forms.Label lblCount;
+    }
+}
+
---
+++ Launcher/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Launcher
+{
+    public partial class FrmMain : Form
+    {
+        private int counter = 0;
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        public int Counter { get => counter; set => counter = value; }
+
+        private void cmdSubmit_Click(object sender, EventArgs e)
+        {
+            Counter = Counter + 1;
+            lblCount.Text = Counter.ToString();
+        }
+    }
+}
